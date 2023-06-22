21255d6cb494314790ffa02434e7d91b5b7aa168
Larry Lai <cgpanda.sg@gmail.com>
2023-06-22 14:01:41 +0800

    eks

--- eks/main.tf
+++ eks/main.tf
@@ -18,7 +18,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.66.0"
+  version = "5.0.0"
 
   name                 = "education-eks"
   cidr                 = "10.0.0.0/16"
@@ -47,20 +47,39 @@ module "vpc" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
-  cluster_version = "1.21"
-  subnets         = module.vpc.private_subnets
+  cluster_version = "1.27"
+  # subnets         = module.vpc.private_subnets
+  // https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+  # subnet_ids      = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
-  version         = "15.1.0"
+  version         = "19.15.3"
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  # workers_group_defaults = {
+  #   root_volume_type = "gp2"
+  # }
+
+  # worker_groups = [
+  #   {
+  #     name                 = "${local.cluster_name}_worker_group"
+  #     instance_type        = "t2.small"
+  #     asg_desired_capacity = 3
+  #   }
+  # ]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
-  worker_groups = [
-    {
-      name                 = "${local.cluster_name}_worker_group"
-      instance_type        = "t2.small"
-      asg_desired_capacity = 3
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+
+      instance_types = ["t2.small"]
     }
-  ]
+  }
 }
